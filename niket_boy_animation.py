baee94ccc8771db3103b0dddddcc1b6812319f0e
Niketraj08 <niketrajkvs@gmail.com>
2026-08-17 15:39:04 +0530

    commit

--- niket_boy_animation.py
+++ niket_boy_animation.py
@@ -102,10 +102,10 @@ def draw_static_scene():
     draw_cloud(-150, 260)
     draw_name_text()
 
-    # ---- Body (shirt) ----
+  -
     draw_rectangle(-40, -20, 80, 100, "#42A5F5", "#1565C0")
 
-    # ---- Legs ----
+   -
     draw_rectangle(-35, -100, 30, 80, "#37474F", "#263238")   # left leg (pant)
     draw_rectangle(5, -100, 30, 80, "#37474F", "#263238")     # right leg (pant)
 
